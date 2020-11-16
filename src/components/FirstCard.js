9ffde42041ce51530ac3e96814fa93077a549e88
Heli Sivunen <heli@mooncat.tv>
2020-11-16 11:33:14 +0000

    Improve API call on FirstCard, not finished yet

--- src/components/FirstCard.js
+++ src/components/FirstCard.js
@@ -5,28 +5,23 @@ import React, { Component } from "react"
 class FirstCard extends Component {
   constructor(props) {
     super(props)
-      this.state = { covidData: [],
-      isHovering: false
-    };
-    this.handleMouseHover = this.handleMouseHover.bind(this);
-  };
-
-  handleMouseHover() {
-    this.setState(this.toggleHoverState);
+      this.state = { covidData: [] }
   }
-
-  toggleHoverState(state) {
-    return {
-      isHovering: !state.isHovering,
-    };
-  }
-
   componentDidMount() {
-    fetch("https://sampo.thl.fi/pivot/prod/api/epirapo/covid19case.json")
+    fetch(`https://sampo.thl.fi/pivot/prod/api/epirapo/covid19case.json`)
     .then(response => response.json())
     .then(data => {
       this.setState({covidData: data})
     })
   }
 
+  render() {
+    return (
+      <div className="firstcard">
+      {this.state.items.map((item) => <p>item</p>)}
+    </div>
+    )
+  }
 }
+
+export default FirstCard
